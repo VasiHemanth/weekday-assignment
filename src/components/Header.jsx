812ce7b9518bff523931f67e2063918447acb8d7
Hemanth Vasi <hemanth.vasi1716@gmail.com>
2024-05-05 10:43:06 +0530

    setting up things

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header-container">
+      <div className="header-content">
+        <h2 className="title">
+          <a href="/">Weekday</a>
+        </h2>
+        <p>Username</p>
+      </div>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./components/Home";
+import Error from "./components/Error";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      // {
+      //   path: '/:job',
+      //   element: <JobDetail />,
+      // },
+      {
+        path: '/contact',
+        element: <div>Contact us</div>,
+      },
+    ],
+  },
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
